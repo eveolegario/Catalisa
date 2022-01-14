425f651547b3c42bff894703ebd1317487a7f011
eveolegario <evelynolegario93@gmail.com>
2022-01-14 16:55:31 -0300

    capturar elementos html manipulaveis

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-script.js
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const status = document.querySelector('#status')
